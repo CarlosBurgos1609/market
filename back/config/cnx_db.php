61ddeb5bf3d599201abddbc7e6c450f095875bbd
Solrac_B <carlitosaburgosj1609@gmail.com>
2023-04-17 08:04:04 -0500

    new create

--- back/config/cnx_db.php
+++ back/config/cnx_db.php
@@ -13,7 +13,7 @@
     if ($conn->connect_error) {
         die("Connection failed:" . $conn->connect_error);
     }
-    else {
-        echo "Connection succesfully";
-    }
+    // else {
+    //     echo "Connection succesfully";
+    // }
 ?>
